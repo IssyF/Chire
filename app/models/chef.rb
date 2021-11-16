83d0819b8d8c3234c2584aa0bf07ba0d8177f44a
Julia Thomas <juliathomas@outlook.com>
2021-11-16 16:25:16 +0000

    Added chef create and new methods and views

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,7 +1,9 @@
 class Chef < ApplicationRecord
+  CUISINES = %w[Chinese Italian Japanese French Indian Thai British American Spanish]
+
   belongs_to :user
   has_many :bookings
 
-  validates :cuisine, presence: true, inclusion: { in: %w[chinese italian japanese french indian thai british american spanish] }
+  validates :cuisine, presence: true, inclusion: { in: CUISINES }
   validates :years_experience, presence: true
 end
