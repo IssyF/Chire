23757fa871f9df13b6fdb558c58aa3770f025582
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-11-16 17:18:28 +0000

    create new bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @chef = Chef.find(params[:chef_id])
+    @booking.chef = @chef
+    if @booking.save
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
 end
