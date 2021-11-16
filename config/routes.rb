585155a51a0da05208aafa98ef2dbdcc11d45b26
Isabel Foo <isabel.foo@hotmail.com>
2021-11-16 11:51:31 +0000

    routes added and checked with Louis

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'users#profile'
+
+  resources :users, only: :show
+
+  resources :chefs, only: %i[new create index show] do
+    resources :bookings, only: %i[new create]
+  end
+
+  # Only code edit and update for a diner if we have time at the end - need update to change confirmed? boolean
+  resources :bookings, only: %i[destroy edit update show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
