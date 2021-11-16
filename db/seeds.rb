78a7599f043c70620fb4ae7cd73f895a65823cc5
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-11-16 16:15:58 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Booking.destroy_all
+Chef.destroy_all
+User.destroy_all
+
+
+cuisine = %w[chinese italian japanese french indian thai british american spanish]
+london_areas = ['Hoxton', 'Brixton', 'Greenwich', 'Chelsea', 'Twickenham', 'Clapham', 'Arsenal', 'Islington']
+
+emma_user = User.create!(first_name: 'Emma', last_name: 'Whittle', email: "emma123@gmail.com", address: '5 Waldon Avenue', password: '123123')
+francis_user = User.create!(first_name: 'Francis', last_name: 'Brown', email: "francis.brown@hotmail.co.uk", address: '65 Cherry Blossom Avenue', password: '123123')
+george_user = User.create!(first_name: 'George', last_name: 'Rush', email: "george1992@gmail.com", address: '40 Kingsland Road', password: '123123')
+ana_user = User.create!(first_name: 'Ana', last_name: 'Gilmore', email: "ana.gilmore@hotmail.com", address: '1 Fleet Street', password: '123123')
+tony_user = User.create!(first_name: 'Tony', last_name: 'Piper', email: "tony1991@gmail.com", address: '541 Oxford Street', password: '123123')
+tori_user = User.create!(first_name: 'Tori', last_name: 'White', email: "tori.white@hotmail.com", address: '12 Wood End', password: '123123')
+
+    4.times do
+  Chef.create!(
+    user_id: User.all.sample.id,
+    cuisine: cuisine.sample,
+    years_experience: rand(1..8)
+  )
+  end
+
+  5.times do
+    Booking.create!(
+    user_id: User.all.sample.id,
+    #start_date_time:
+    #end_date_time:
+    chef_id: Chef.where("id != user_id").sample.id
+    )
+end
